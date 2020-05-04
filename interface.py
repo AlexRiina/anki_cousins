0855fa1f446c448c6b9196aab879ed201d6b878e
Alex Riina <alex.riina@gmail.com>
2020-05-03 20:43:10 -0400

    both grid and list view working! (ish)

--- interface.py
+++ interface.py
@@ -2,20 +2,21 @@ from typing import List
 from functools import partial
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QAction,
     QPushButton,
     QDialog,
     QDialogButtonBox,
+    QGridLayout,
     QVBoxLayout,
     QFormLayout,
     QComboBox,
+    QCheckBox,
     QDoubleSpinBox,
     QLineEdit,
     QLabel,
 )
 from anki.hooks import addHook
 from anki.collection import _Collection
-from aqt import mw
+from aqt import mw  # type: ignore
 
 
 def show_settings_dialog() -> None:
@@ -38,9 +39,20 @@ def show_settings_dialog() -> None:
     buttons.rejected.connect(dialog.reject)
     buttons.setOrientation(Qt.Horizontal)
 
+    grid = QGridLayout()
+
+    for index, label in enumerate([
+            QLabel("on note"),
+            QLabel("match field"),
+            QLabel("to note"),
+            QLabel("match field"),
+            QLabel("matcher"),
+            QLabel("similarity")]):
+        grid.addWidget(label, 0, index)
+
     match_rules: List[MatchRuleForm] = []
     for stored_values in col.conf.get('anki_cousins', []):
-        form = MatchRuleForm(note_types)
+        form = MatchRuleForm(note_types, grid)
 
         try:
             form.set_values(*stored_values)
@@ -48,31 +60,40 @@ def show_settings_dialog() -> None:
             col.log("invalid cousin matching config")
             continue
 
-        forms.addLayout(form.layout)
+        if grid:
+            pass
+        else:
+            forms.addLayout(form.layout)
         match_rules.append(form)
 
     def append_row():
-        form = MatchRuleForm(note_types)
-        forms.addLayout(form.layout)
+        form = MatchRuleForm(note_types, grid)
+        if grid:
+            pass
+        else:
+            forms.addLayout(form.layout)
         match_rules.append(form)
 
     append.clicked.connect(append_row)
 
     dialog_layout.addLayout(forms)
+    if grid:
+        dialog_layout.addLayout(grid)
     dialog_layout.addWidget(append)
     dialog_layout.addWidget(buttons)
 
     print(col.conf.get('anki_cousins'))
 
     if dialog.exec_():
-        col.conf['anki_cousins'] = [match_rule.get_values() for match_rule in match_rules if match_rule.is_valid()]
+        col.conf['anki_cousins'] = [match_rule.get_values() for match_rule in
+                                    match_rules if match_rule.is_valid()]
         col.setMod()
 
 
 class MatchRuleForm:
     layout: QFormLayout
 
-    def __init__(self, note_types):
+    def __init__(self, note_types, grid):
         self._my_note_type = QComboBox()
         for note_type, note_id in note_types.items():
             self._my_note_type.addItem(note_type, note_id)
@@ -97,13 +118,28 @@ class MatchRuleForm:
         self._threshold.setSingleStep(0.05)
         self._threshold.setValue(0.95)
 
-        self.layout = QFormLayout()
-        self.layout.addRow(QLabel("on note"), self._my_note_type)
-        self.layout.addRow(QLabel("match field"), self._my_note_field)
-        self.layout.addRow(QLabel("to note"), self._other_note_type)
-        self.layout.addRow(QLabel("match field"), self._other_note_field)
-        self.layout.addRow(QLabel("matcher"), self._matcher)
-        self.layout.addRow(QLabel("similarity threshold"), self._threshold)
+        self._delete = QCheckBox('delete?')
+
+        if False:
+            self.layout = QFormLayout()
+            self.layout.addRow(QLabel("on note"), self._my_note_type)
+            self.layout.addRow(QLabel("match field"), self._my_note_field)
+            self.layout.addRow(QLabel("to note"), self._other_note_type)
+            self.layout.addRow(QLabel("match field"), self._other_note_field)
+            self.layout.addRow(QLabel("matcher"), self._matcher)
+            self.layout.addRow(QLabel("similarity threshold"), self._threshold)
+            self.layout.addRow(self._delete)
+        else:
+            row = grid.rowCount()
+
+            for index, field in enumerate([self._my_note_type,
+                                           self._my_note_field,
+                                           self._other_note_type,
+                                           self._other_note_field,
+                                           self._matcher,
+                                           self._threshold,
+                                           self._delete]):
+                grid.addWidget(field, row, index)
 
     def set_values(self,
                    my_note_type_value,
@@ -121,7 +157,8 @@ class MatchRuleForm:
             self._my_note_field.setText(my_note_field_value)
 
         if other_note_type_value:
-            self._other_note_type.setCurrentIndex(self._other_note_type.findData(other_note_type_value))
+            self._other_note_type.setCurrentIndex(
+                self._other_note_type.findData(other_note_type_value))
 
         if other_note_field_value:
             self._other_note_field.setText(other_note_field_value)
@@ -143,7 +180,8 @@ class MatchRuleForm:
         )
 
     def is_valid(self):
-        return all(value not in {None, ""} for value in self.get_values())
+        return not self._delete.isChecked() and \
+            all(value not in {None, ""} for value in self.get_values())
 
 
 @partial(addHook, "profileLoaded")
