8255ffc44668bb1b2955f8e7d562d0c0976bfef2
Alex Riina <alex.riina@gmail.com>
2020-05-23 11:35:13 -0400

    bump version for beta release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="anki_cousins",
-    version="0.1",
+    version="0.2",
     tests_require=["PyQt5-stubs" "anki", "black", "flake8", "isort", "mypy"],
     packages=find_packages("src"),
     package_dir={"": "src"},
