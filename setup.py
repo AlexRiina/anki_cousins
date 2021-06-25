b7be37907f928f7e0693f9b584f1c41c923eae25
Alex Riina <alex.riina@gmail.com>
2021-06-24 20:17:20 -0400

    add manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="anki_cousins",
-    version="0.4",
+    version="0.5",
     tests_require=["PyQt5-stubs" "anki", "black", "flake8", "isort", "mypy"],
     packages=["src"],
 )
